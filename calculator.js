5748d82ff8538efcb7a0b6b54a8269704909d17b
fleeee1 <160426830+fleeee1@users.noreply.github.com>
2024-07-02 17:08:37 -0400

    add operate function

--- calculator.js
+++ calculator.js
@@ -1,11 +1,21 @@
-const divide = (a, b) => a / b;
-const multiply = (a, b) => a * b;
-const subtract = (a, b) => a - b;
 const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
-var firstNumber = num1
-var symbol = operator
-var secondNumber = num2
+const operate = (operator, num1, num2) => {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default: 
+            return null
+    }
+};
 
-let calculate = operate => (num1, num2)
-    
+let result = operate(operator, firstNumber, secondNumber);
