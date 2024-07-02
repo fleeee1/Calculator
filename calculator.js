7bde95d4cc6d4a6db65fef9c66d8731420b5676f
fleeee1 <160426830+fleeee1@users.noreply.github.com>
2024-07-02 15:57:10 -0400

    add pseudocode with basic calculation functions

---
+++ calculator.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+// let division = (a, b) => a / b;
+
+// let num1 = 48;
+// let num2 = 8;
+// let quotient = division(num1, num2);
+
+// console.log(quotient);
+
+
+
+
+// let multiplication = (a, b) => a * b;
+
+// let num1 = 4;
+// let num2 = 3;
+// let product = multiplication(num1, num2);
+
+// console.log(product);
+
+
+
+
+// let subtraction = (a, b) => a - b;
+
+// let num1 = 704;
+// let num2 = 28;
+// let difference = subtraction(num1, num2);
+
+// console.log(difference);
+
+
+
+
+
+// let addition = (a, b) => a + b;
+
+// let num1 = 47909;
+// let num2 = 30;
+// let sum = addition(num1, num2);
+
+// console.log(sum);
